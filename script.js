08a7ebaefee8a78672e13e2521606608bc033f43
xiTTzP <es.vito@hotmail.com>
2024-04-22 14:09:18 -0300

    adicionado sessao de avaliacao e melhorias na responsividade

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const hamburger = document.querySelector(".menu-burguer");
 const bloco2 = document.querySelector("#bloco-2");
+const menuitem = document.getElementsByClassName("a-menu");
 
-if(hamburger){
 
-    hamburger.addEventListener("click", () =>{
+
+if (hamburger && menuitem) {
+
+    // hamburger.classList.add("active");
+    //     bloco2.classList.add("active");
+    // Quando clicado o menu aparece para o usuario
+    hamburger.addEventListener("click", () => {
+
         hamburger.classList.toggle("active");
         bloco2.classList.toggle("active");
     });
+
+    // Quando clicado ele esconde o nav
+    menuitem.addEventListener("click", () => {
+
+        bloco2.classList.toggle("active");
+        hamburger.classList.toggle("active");
+
+
+    });
 }
+
