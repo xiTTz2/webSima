f12aad6c132a3b07016fe8c7575e44f7f83a2357
xiTTzP <es.vito@hotmail.com>
2024-04-28 11:40:27 -0300

    mais uma correcao de responsividade

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const hamburger = document.querySelector(".menu-burguer");
 const bloco2 = document.querySelector("#bloco-2");
-
+const menu = document.querySelector("#bloco a");
 
 
 
 if (hamburger) {
 
+    
     // hamburger.classList.add("active");
     //     bloco2.classList.add("active");
     // Quando clicado o menu aparece para o usuario
     hamburger.addEventListener("click", () => {
 
         hamburger.classList.toggle("active");
+        bloco2.classList.toggle("active");
+    });
+
+    
+
+}
+
+if(menu){
+    menu.addEventListener("click", () =>{
+        // hamburger.classList.toggle("activate");
         bloco2.classList.toggle("active");
     });
 
